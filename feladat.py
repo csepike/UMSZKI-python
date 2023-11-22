853cbe970e3c19f1e683b0ec2426eaf9d452f3b3
csepike <cseplo.balu@gmail.com>
2023-11-22 13:53:25 +0100

    1. feladat

--- feladat.py
+++ feladat.py
@@ -1,14 +1,23 @@
 termek = ["Sertéskaraj", "Gouda sajt", "Dobozos sajt", "Csirkemell filé", "Mirelit Pizza", "Vaj", "Edámi sajt", "Szőlő", "Paradicsom", "Szalámi", "Rizs", "Banán", "Zöldségmix", "Cola", "Hagyma", "Kenyér", "Narancs", "Alma", "Napraforgóolaj", "Spaghetti tészta", "Teljes tej", "Joghurt", "Almalé", "Burgonya", "Babkonzerv", "Cukor", "WC-papír", "Búzaliszt", "Friss tojás"]
-egyseg = ["kg", "450 g", "400 g", "kg", "db", "250 g", "200 g", "1 kg", "1 kg", "100 g", "1 kg", "1 kg", "1 kg", "2l", "1 kg", "1 kg", "1 kg", "1 kg", "1 l", "500 g", "1 l", "500 ml", "1 l", "1 kg", "240 g", "1 kg", "tekercs", "1 kg", "db"]
+egyseg = ["kg",          "450 g",      "400 g",         "kg", "db", "250 g", "200 g", "1 kg", "1 kg", "100 g", "1 kg", "1 kg", "1 kg", "2l", "1 kg", "1 kg", "1 kg", "1 kg", "1 l", "500 g", "1 l", "500 ml", "1 l", "1 kg", "240 g", "1 kg", "tekercs", "1 kg", "db"]
 
 # 1. feladat
 # ▪ Kérje be a felhasználótól egy keresett termék nevét!
 # ▪ Ellenőrizze, hogy az megtalálható-e a termékek között!
 # ▪     Ha igen, akkor állapítsa meg az indexét és írja ki a kiszerelés egységét!
 # ▪     Ha nem, akkor a parancssoron a „Nincs ilyen termék” üzenet jelenjen meg!
+kerem = input("Mit kérsz?")
+if kerem in termek:
+    index = termek.index(kerem)
+    kiszerelese = egyseg[index]
+    print(kiszerelese)           
+else:
+    print("Nincs ilyen termék")
 
 december = [ 2550, 1602, 1140, 1620, 1320, 682, 750, 1700, 1000, 693, 651, 686, 805, 702, 600, 582, 798, 648, 522, 605, 550, 567, 286, 350, 351, 250, 154, 135, 50 ]
 november = [ 2400, 1800, 1500, 1500, 1500, 1100, 1000, 1000, 1000, 770, 700, 700, 700, 600, 600, 600, 600, 600, 580, 550, 440, 540, 440, 350, 270, 250, 220, 150, 80 ]
+
+
 
 # 2. feladat
 # ▪ Vezessen be egy üres listát 'változás' azonosítóval!
@@ -19,6 +28,7 @@ november = [ 2400, 1800, 1500, 1500, 1500, 1100, 1000, 1000, 1000, 770, 700, 700
 #     ▪ Kerekítse két tizedesjegyre a 'round(százalék, 2)' függvény használatával!
 #     ▪ Füzze az eredményt a 'változás' lista végéhez!
 # ▪ A ciklus után írja képernyőre a kiszámolt adatokat!
+
 
 
 # 3. feladat
